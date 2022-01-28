0debd47e3347cd13c41c124aa559472e0685a466
PennyJim <40438818+PennyJim@users.noreply.github.com>
2022-01-28 00:24:23 -0700

    Created rateLimiter.js exports

--- rateLimiter.js
+++ rateLimiter.js
@@ -44,7 +44,7 @@ db.prepare(`
         UPDATE
             limits
         SET
-            use_interval = NEW.use_per_hour / 60.0;
+            use_interval = 60.0 / NEW.use_per_hour;
     END;
 `).run();
 //Make sure it matches the config
@@ -52,9 +52,9 @@ db.prepare(`
     INSERT INTO limits
         (limit_id, limit_type, max_uses, use_per_hour, use_interval)
     VALUES
-        (1, $color, $color_max, $color_per_hour, $color_per_hour / 60.0),
-        (2, $clean, $clean_max, $clean_per_hour, $clean_per_hour / 60.0),
-        (3, $config, $config_max, $config_per_hour, $config_per_hour / 60.0)
+        (1, $color, $color_max, $color_per_hour, 60.0 / $color_per_hour),
+        (2, $clean, $clean_max, $clean_per_hour, 60.0 / $clean_per_hour),
+        (3, $config, $config_max, $config_per_hour, 60.0 / $config_per_hour)
 `).run(config.ratelimits);
 
 //Make the buckets table
@@ -63,7 +63,7 @@ db.prepare(`
         user_id CHAR(18) NOT NULL,
         limit_id TINYINT NOT NULL,
         uses_left SMALLINT,
-        last_updated DATETIME,
+        last_token DATETIME,
         FOREIGN KEY (limit_id) REFERENCES limits(limit_id)
         PRIMARY KEY (user_id, limit_id)
         CHECK(uses_left >= 0)
@@ -86,15 +86,19 @@ db.prepare(`
                     limits
                 WHERE
                     limit_id = NEW.limit_id
-            )
-        WHERE NEW.uses_left > (
-            SELECT
-                max_uses
-            FROM
-                limits
-            WHERE
-                limit_id = NEW.limit_id
-        );
+            ),
+            last_token = DATETIME('now')
+        WHERE
+            user_id = NEW.user_id
+            AND limit_id = NEW.limit_id
+            AND NEW.uses_left > (
+                SELECT
+                    max_uses
+                FROM
+                    limits
+                WHERE
+                    limit_id = NEW.limit_id
+            );
     END;
 `).run();
 //Autofill the bucket information
@@ -113,30 +117,128 @@ db.prepare(`
                 WHERE
                     lim.limit_id = NEW.limit_id
             ),
-            last_updated = datetime('now')
+            last_token = DATETIME('now')
         WHERE
             user_id = NEW.user_id
             AND limit_id = NEW.limit_id;
     END;
 `).run();
 //Make db use concurrent
-db.pragma('journal_mode = WAL');
+// db.pragma('journal_mode = WAL'); //TODO: Renable this
 
 
 const newBucket = db.prepare(`
     INSERT INTO buckets (
         user_id,
-        limit_id,
-        uses_left,
-        last_updated
+        limit_id
     )
     VALUES (
         $user_id,
-        $limit_id,
-        null,
-        null
+        $limit_id
     )
 `)
+const checkBucket = db.prepare(`
+    SELECT
+        *
+    FROM
+        buckets
+    WHERE
+        user_id = $user_id
+        AND limit_id = $limit_id
+`).pluck(true)
+const updateBucket = db.prepare(`
+    WITH calc AS
+        (SELECT
+            FLOOR((JULIANDAY('now') - JULIANDAY(buc.last_token)) * 24 * lim.use_per_hour) AS new_tokens,
+            DATETIME(buc.last_token, (FLOOR((JULIANDAY('now') - JULIANDAY(buc.last_token))
+                * 24 * lim.use_per_hour) * lim.use_interval) || ' minutes') AS new_token_time
+        FROM
+            buckets buc
+        JOIN
+            limits lim
+        ON
+            lim.limit_id = buc.limit_id
+        WHERE
+            buc.user_id = $user_id
+            AND buc.limit_id = $limit_id)
+    UPDATE
+        buckets
+    SET
+        uses_left = uses_left + (
+            SELECT
+                new_tokens
+            FROM
+                calc
+            ),
+        last_token = (
+            SELECT
+                new_token_time
+            FROM
+                calc
+            )
+    WHERE
+        user_id = $user_id
+        AND limit_id = $limit_id
+        AND 0 < (
+            SELECT
+                new_tokens
+            FROM
+                calc
+            )
+`)
+const useBucket = db.prepare(`
+    UPDATE buckets
+    SET
+        uses_left = uses_left - 1
+    WHERE
+        user_id = $user_id
+        AND limit_id = $limit_id
+`)
+function checkLimit(user_id, limit_id) {
+    let idObj = {user_id: user_id, limit_id: limit_id}
+    updateBucket.run(idObj);
+    if (!checkBucket.get(idObj)) newBucket.run(idObj);
+}
+function useLimit(user_id, limit_id) {
+    //Allow you to pass a GuildManager, MemberManager, or UserManager object
+    if (user_id.id !== undefined) user_id = user_id.id;
+    checkLimit(user_id, limit_id);
+    try {
+        useBucket.run({user_id: user_id, limit_id: limit_id});
+    } catch (error) {
+        return false;
+    }
+    return true;
+}
+exports.useColor = (user_id) => {
+    return useLimit(user_id, 1);
+}
+exports.useClean = (user_id) => {
+    return useLimit(user_id, 2);
+}
+exports.useConfig = (user_id) => {
+    return useLimit(user_id, 3);
+}
+
+// Basic guilds for debug
+const devGuild = "770338797543096381"; //Dev Server
+checkLimit(devGuild, 1);
+checkLimit(devGuild, 2);
+checkLimit(devGuild, 3);
+console.log("Color:", exports.useColor(devGuild));
+console.log("Color:", exports.useColor(devGuild));
+console.log("Color:", exports.useColor(devGuild));
+console.log("Clean:", exports.useClean(devGuild));
+console.log("Clean:", exports.useClean(devGuild));
+console.log("Clean:", exports.useClean(devGuild));
+console.log("Config:", exports.useConfig(devGuild));
+console.log("Config:", exports.useConfig(devGuild));
+console.log("Config:", exports.useConfig(devGuild));
+// checkLimit(devGuild, 1);
+// checkLimit(devGuild, 2);
+// checkLimit(devGuild, 3);
+
+//Table Setup
 /*
 
 CREATE TABLE IF NOT EXISTS limits (
@@ -159,7 +261,7 @@ CREATE TABLE IF NOT EXISTS buckets (
     user_id CHAR(18) NOT NULL,
     limit_id TINYINT NOT NULL,
     uses_left SMALLINT,
-    last_updated DATETIME,
+    last_token DATETIME,
     FOREIGN KEY (limit_id) REFERENCES limits(limit_id)
     PRIMARY KEY (user_id, limit_id)
 ) WITHOUT ROWID
