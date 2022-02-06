bdbbd9af15d43b2bb8a0e19b354c2640957f8ebf
PennyJim <40438818+PennyJim@users.noreply.github.com>
2022-02-05 21:01:09 -0700

    Started banColor and config

---
+++ commands/banColor.js
@@ -0,0 +1,38 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const settings = require('../settings.js');
+const logger = require("../logger.js");
+
+exports.msgrun = async (client, message, args) => {
+    message.reply({content: "My ping is \`" + client.ws.ping + " ms\`", ephemeral: true});
+}
+
+exports.slashrun = async (client, interaction) => {
+    if (!interaction.inGuild())
+        return interaction.reply({content: "This only works in guilds", ephemeral: true});
+    if (interaction.member.id !== interaction.guild.ownerId &&
+        !(settings.getCanAdminConfig(interaction.guild.id) && interaction.memberPermissions.has("ADMINISTRATOR")))
+        return interaction.reply({content: "You do not have permission to do this", ephemeral: true});
+    
+    interaction.reply({content: "My ping is \`" + client.ws.ping + " ms\`", ephemeral: true});
+}
+
+exports.help = {
+    name:"bancolor",
+    limit: "config",
+    limitScope: "guild"
+}
+
+exports.generateCommand = (isTest = false) => {
+    if (!isTest) {
+        return [
+
+        ]
+    } else {
+        return [
+            new SlashCommandBuilder()
+                .setName(exports.help.name)
+                .setDescription("(Test) Ban a color from use")
+                .toJSON()
+        ]
+    }
+}
