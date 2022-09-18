3dec66cbc51cabe9def7b7586ca6a018abbc9806
PennyJim <40438818+PennyJim@users.noreply.github.com>
2022-09-18 00:14:48 -0600

    A typo on 1bc89ac8adcf59f33921fe02667d05f9efdcc795

--- data/settings.js
+++ data/settings.js
@@ -102,7 +102,7 @@ function newGuild(guild) {
     let defaultGuild = getGuild.get({ guild_id: defaultID })
     if (guild.minrole === undefined) {guild.minrole = guild.guild_id}
     if (guild.maxroles === undefined) {guild.maxroles = defaultGuild.maxroles}
-    if (guild.color_threshhold === undefined) {guild.color_threshhold = defaultGuild.color_threshhold}
+    if (guild.color_threshold === undefined) {guild.color_threshold = defaultGuild.color_threshold}
     if (guild.can_admin_config === undefined) {guild.can_admin_config = defaultGuild.can_admin_config}
     addGuild.run(guild);
 }
